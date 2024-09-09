6cd208fcbb86510c8a8671fbd4b7c4bd12e37573
Yang Wang <20112222@tafe.wa.edu.au>
2024-09-09 16:34:01 +0800

    Change challenge 2.

--- session-08/in-class-code/my-react-app/UserObject.js
+++ session-08/in-class-code/my-react-app/UserObject.js
@@ -2,25 +2,29 @@ import React from 'react';
 import { getUserInfo, formatSkills } from './utils'; // Step 1 fix the import statement
 
 function UserProfile(props) {
+
   // Step 2 create object correctly
-  const user = new Object;
-    firstName = 'John';
-    lastName = 'Doe';
-    age = 30;
-    isStudent = false;
-    skills = ['JavaScript', 'React', 'CSS'];
+  const user = {
+    firstName : 'John',
+    lastName : 'Doe',
+    age : 30,
+    isStudent : false,
+    skills : ['JavaScript', 'React', 'CSS']}
+
+    const {firstName, lastName, age, isStudent, skills} = props;
+
   };
 
   // Step 3 fix the map method & fix duplicated method
-  const formatSkills = function(skills) {
-    return skills.map(() => skills.touppercase());
-  };
+  // const formatSkills = function(skills) {
+  //   return skills.map(skills => skills.touppercase());
+  // };
 
   // Step 4 destructure props properly
-  const [firstName, lastName, age, isStudent, skills] = props;
+  
 
   // Step 5 Fix the ternary operator
-  const greetingMessage = $user.firstName ? `Hello, ${user.firstName} `: 'Hello, Guest!';
+  const greetingMessage = user.firstName ? `Hello, ${user.firstName} `: 'Hello, Guest!';
 
   // Step 6 Fix the duplicated method
   const formattedSkills = formatSkills(user.skills);
